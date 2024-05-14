926e3cd966cf1e679224a8e610228e23f9fd6968
vavasoftdev01 <vavasoftdev@outlook.com>
2024-05-14 17:08:12 +0800

    Finish RNG functionality for results api

--- src/results/entities/result.entity.ts
+++ src/results/entities/result.entity.ts
@@ -1,1 +1,73 @@
-export class Result {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+//@Entity({ name: 'dbo.3M_powerball', synchronize: false})
+export class Result {
+    // @PrimaryGeneratedColumn({})
+    // idx: number;
+
+    // @Column({ nullable: true})
+    // game: string;
+
+    // @Column({ nullable: false})
+    // id: number;
+
+    // @Column({ nullable: true})
+    // round: string;
+
+    // @Column({ nullable: true })
+    // dt: string;
+
+    // @Column({ nullable: true })
+    // sdate: string;
+
+    // @Column({ nullable: true })
+    // edate: string;
+
+    // @Column({ nullable: true })
+    // pb: string;
+
+    // @Column({ nullable: true })
+    // num1: string;
+
+    // @Column({ nullable: true })
+    // num2: string;
+
+    // @Column({ nullable: true })
+    // num3: string;
+
+    // @Column({ nullable: true })
+    // num4: string;
+
+    // @Column({ nullable: true })
+    // num5: string;
+
+    // @Column({ nullable: false })
+    // num_sum: number;
+
+    // @Column({ nullable: true })
+    // num_sum_sec: string;
+
+    // @Column({ nullable: true })
+    // num_sum_odd: string;
+
+    // @Column({ nullable: true })
+    // pb_odd: string;
+
+    // @Column({ nullable: true })
+    // num_odd: string;
+
+    // // Date Constraints..
+    // @Column({ nullable: true, type: 'date' })
+    // regdate: string;
+
+    // @Column({ nullable: true, type: 'date' })
+    // modifydate: string;
+
+    // @Column({ nullable: true, type: 'date' })
+    // accountdate: string;
+
+    // @Column({ nullable: true, type: 'date' })
+    // void: string;
+
+
+}
--- src/results/results.controller.ts
+++ src/results/results.controller.ts
@@ -1,11 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res, HttpStatus } from '@nestjs/common';
 import { ResultsService } from './results.service';
 import { CreateResultDto } from './dto/create-result.dto';
 import { UpdateResultDto } from './dto/update-result.dto';
+import { Response } from 'express';
 
 @Controller('results')
 export class ResultsController {
   constructor(private readonly resultsService: ResultsService) {}
+
+  @Get('/generate')
+  generateResult() {
+    let draw_results = this.uniqueRNG(28, 6)
+    // check if all results are double digit hence the powerball should be not greater than 9
+    const checker = draw_results.every((element) => {
+      return element >= 10 
+    })
+
+    if(checker) {
+      draw_results.pop()
+      draw_results.push({'powerball' :this.uniqueRNG(9, 1)[0]})
+    }
+
+    // Assign powerball to the first element that is <= 10.
+    for (let index = 0; index < draw_results.length; index++) {
+      if(draw_results[index] <= 9) {
+        let cont = draw_results[index]
+        draw_results[index] = {'powerball_def': cont}
+        break;
+      }
+    }
+
+    return draw_results;
+  }
+
+  private uniqueRNG(max_number_limit, iteration) {
+    const results = [];
+    let unique_results = []
+    // Draw results ..
+    for (let index = 1; index <= 15; index++) {
+      results.push(Math.floor(Math.random() * max_number_limit) + 1)
+      // Unique
+      unique_results = [... new Set(results)]
+    }
+
+    return unique_results.slice(0, iteration);
+  }
 
   @Post()
   create(@Body() createResultDto: CreateResultDto) {
