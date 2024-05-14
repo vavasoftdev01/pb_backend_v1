3561d8366ca227672650ad5b5d1a4bab5d2115f6
vavasoftdev01 <vavasoftdev@outlook.com>
2024-05-14 11:28:05 +0800

    install typeorm and establish connection to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ResultsModule } from './results/results.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ResultsModule,
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false, //use this with development enviroment
+      options: { encrypt: false },
+    }),
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
 export class AppModule {}
