bd719bb79eab2c0100ef526e23657ac2fe83e3ec
vavasoftdev01 <vavasoftdev@outlook.com>
2024-06-07 14:02:24 +0800

    getDailyPBStatitistics

--- src/statistics/statistics.gateway.ts
+++ src/statistics/statistics.gateway.ts
@@ -23,6 +23,7 @@ export class StatisticsGateway {
     let previousValue = null;
     let container = [];
     
+    
     for (let key in data) {
         if (data[key]['pb_odd'] === previousValue) {
             container.push(data[key]);
@@ -41,6 +42,63 @@ export class StatisticsGateway {
     }
 
     return results;
+  }
+
+  @SubscribeMessage('getDailyPBStatistics')
+  async getDailyPBStatitistics() {
+    const data = await this.statisticsService.findAll();
+
+    let results = [];
+    let previousValue = null;
+    let container = [];
+    let evenOddOverUnderCounter = 0;
+    let streakCounter = 0;
+    let nonStreakCounter = 0;
+
+
+    for (let key in data) 
+    {
+        // Streak
+        if (data[key]['pb_odd'] === previousValue) 
+        {
+            container.push(data[key]);
+            evenOddOverUnderCounter = (data[key]['pb_odd'] === 'E') ? evenOddOverUnderCounter + 1 : evenOddOverUnderCounter;   
+        } 
+        else 
+        {
+          // Non-streak
+            if (container.length > 0) {
+                results.push(container);
+                container = [];
+                
+            }
+
+            container.push(data[key]);
+        }
+
+      previousValue = data[key]['pb_odd'];
+    }
+    
+    if (container.length > 0) {
+      results.push(container);
+    }
+
+      collect(results).map((result) => {
+        if(result.length > 1) {
+          streakCounter = streakCounter + 1
+        }
+        nonStreakCounter = nonStreakCounter + 1
+      });
+
+    return [
+      {
+        'streak_count': streakCounter,
+        'non_streak_count': nonStreakCounter,
+        'even_count': evenOddOverUnderCounter,
+        'total_results': data.length,
+        'results': results
+      }
+    ];
   }
 
   @SubscribeMessage('findOneStatistic')
